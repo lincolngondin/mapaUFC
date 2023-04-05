ba50c0ca468654c49c5454c6b3265908b661c6b1
lincolngondin <lincolngondim@gmail.com>
2023-04-05 10:50:55 -0300

    Adicionado nomes no famed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* Global states */
 let levelSelecionado = "0";
-
+let campusSelecionado = 0; // 0 = mucabinho 1= famed 2 = nenhum
+let zoomAtual = 0;
 
 let mapOptions = {
     zoomControl:false,
@@ -411,8 +412,6 @@ addIconsInLayerGroup();
 
 //quando botao de mudar o level é clicado esse funcao e chamada
 const fn = ()=>{
-
-
     map.removeControl(myL);
     map.addControl(myL);
     geo.clearLayers();
@@ -420,20 +419,27 @@ const fn = ()=>{
     famedGeojson.clearLayers();
     famedGeojson.addData(famed);
     for(nivel of niveisMucab){
-        andares[nivel].remove();
+        nomeLocaisMucab[nivel].remove();
     }
     if(map.getZoom() > 21){
-        andares[levelSelecionado].addTo(map);
+        nomeLocaisMucab[levelSelecionado].addTo(map);
     }
     
     addIconsInLayerGroup();
+    updateNomes(zoomAtual);
 }
 
 /* Extende a classe controle para criar um controle de andar */
 const LayerButton = L.Control.extend({
     onAdd: function(){
         let buttonContainer = L.DomUtil.create("div", "level-buttons");
-        let levels = ObterNiveis(blocoEngenharia.features);
+        let levels;
+        if(campusSelecionado == 0){
+            levels = ObterNiveis(blocoEngenharia.features);
+        }
+        else{
+            levels = ObterNiveis(famed.features);
+        }
         for(let i = 0; i < levels.length; i++){
             let bt = L.DomUtil.create("div", "level-buttons__button", buttonContainer);
             L.DomEvent.on(bt, "click", ()=>{
@@ -458,16 +464,24 @@ const myL = new LayerButton({
 map.addControl(myL);
 
 const niveisMucab = ObterNiveis(blocoEngenharia.features);
-const andares = {};
-const copiaAndares = {};
+const niveisFamed = ObterNiveis(famed.features);
+const nomeLocaisMucab = {};
+const nomeLocaisFamed = {};
+
+/* Cria os layers groups para cada campus */
 for(let i = 0; i < niveisMucab.length; i++){
-    andares[niveisMucab[i]] = L.layerGroup([], {
+    nomeLocaisMucab[niveisMucab[i]] = L.layerGroup([], {
+        interactive: false
+    });
+}
+for(let i = 0; i < niveisFamed.length; i++){
+    nomeLocaisFamed[niveisFamed[i]] = L.layerGroup([], {
         interactive: false
     });
 }
 
+/* Cria os marcadores dos nomes dos locais */
 for(let feature of blocoEngenharia.features){
-    
     if(feature.properties.name !== undefined && feature.properties.indoor === "room" && feature.geometry.type === "LineString" && feature.properties.level !== undefined){
         let c = CentroGeometrico(feature.geometry.coordinates);
         let icon = L.divIcon({
@@ -481,7 +495,24 @@ for(let feature of blocoEngenharia.features){
             interactive:false,
             keyboard: false
         });
-        andares[feature.properties.level].addLayer(mrr);
+        nomeLocaisMucab[feature.properties.level].addLayer(mrr);
+    }
+}
+for(let feature of famed.features){
+    if(feature.properties.name !== undefined && feature.properties.indoor === "room" && feature.geometry.type === "LineString" && feature.properties.level !== undefined){
+        let c = CentroGeometrico(feature.geometry.coordinates);
+        let icon = L.divIcon({
+            html: `<p>${feature.properties.name}</p>`,
+            className: "names",
+            iconSize: L.point(100, 100),
+            iconAnchor: L.point(50,50)
+        });
+        let mrr = L.marker([c.lng, c.lat], {
+            icon:icon,
+            interactive:false,
+            keyboard: false
+        });
+        nomeLocaisFamed[feature.properties.level].addLayer(mrr);
     }
 }
 
@@ -490,19 +521,44 @@ map.on("click", function(evento){
     console.log(evento.latlng)
 })
 
-map.on("zoom", function(evento){
-    let zoom = evento.target._zoom;
+function updateNomes(zoom){
     if(zoom < 21){
-        for(nivel of niveisMucab){
-            andares[nivel].remove();
+        if(campusSelecionado == 0){
+            for(nivel of niveisMucab){
+                nomeLocaisMucab[nivel].remove();
+            }
         }
+
+        if(campusSelecionado == 1){
+            for(nivel of niveisFamed){
+                nomeLocaisFamed[nivel].remove();
+            }
+        }
+
     }
     else{
-        for(nivel of niveisMucab){
-            andares[nivel].remove();
+
+        if(campusSelecionado == 0){
+            for(nivel of niveisMucab){
+                nomeLocaisMucab[nivel].remove();
+            }
+            nomeLocaisMucab[levelSelecionado].addTo(map);
         }
-        andares[levelSelecionado].addTo(map);
+
+        if(campusSelecionado == 1){
+            for(nivel of niveisFamed){
+                nomeLocaisFamed[nivel].remove();
+            }
+            nomeLocaisFamed[levelSelecionado].addTo(map);
+        }
+
     }
+    
+}
+map.on("zoom", function(evento){
+    let zoom = evento.target._zoom;
+    zoomAtual = zoom;
+    updateNomes(zoom);
     if(zoom < 17){
         famedGeojson.remove();
         geo.remove();
@@ -514,12 +570,13 @@ map.on("zoom", function(evento){
 });
 
 /* Funcao chamada quando o mapa é movido */
+/*
 let removedLayers = [];
 function MOVEEND(evento){
     let limiteAtual = map.getBounds();
-    let layers = andares[levelSelecionado].getLayers();
+    let layers = nomeLocaisMucab[levelSelecionado].getLayers();
     for(l of removedLayers){
-        andares[levelSelecionado].addLayer(l);
+        nomeLocaisMucab[levelSelecionado].addLayer(l);
     }
     removedLayers = [];
     for(l of layers){
@@ -528,17 +585,15 @@ function MOVEEND(evento){
             l.remove(layers);
         } 
     }
-    
-    
 }
 
 map.on("moveend", MOVEEND);
+*/
 
-let centroMucambinho = L.latLng([-3.6934337,-40.354892]);
-let limitesMucabinho = centroMucambinho.toBounds(210);
-let centroFamed = L.latLng([-3.681489,-40.336803]);
-let limitesFamed = centroFamed.toBounds(150);
-
+const centroMucambinho = L.latLng([-3.6934337,-40.354892]);
+const limitesMucabinho = centroMucambinho.toBounds(210);
+const centroFamed = L.latLng([-3.681489,-40.336803]);
+const limitesFamed = centroFamed.toBounds(150);
 
 map.on("move", function(evento){
     if(!map.getBounds().intersects(limitesMucabinho)){
@@ -555,7 +610,29 @@ map.on("move", function(evento){
     }
 })
 
+map.on("moveend", function(evento){
+    let zoom = evento.target._zoom;
+    updateNomes(zoom);
+    if(campusSelecionado != 0){
+        if(map.getBounds().intersects(limitesMucabinho)){
+            campusSelecionado = 0;
+            levelSelecionado = "0";
+            fn();
+        }
+    }
 
+    if(campusSelecionado != 1){
+        if(map.getBounds().intersects(limitesFamed)){
+            campusSelecionado = 1;
+            levelSelecionado = "0";
+            fn();
+        }
+    }
+
+});
+
+
+/* Tile Layers */
 
 const cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
 var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
@@ -578,35 +655,13 @@ const openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.pn
     maxNativeZoom: 18,
 });
 
-/*
-var myIcon = L.icon({
-    iconUrl: 'icons/a1.png',
-    iconSize: [20, 20],
-});
-
-let dv = L.popup([-3.6933203,-40.3543455], {
-    content: `
-        <div>
-            <h3>Estado de acessibilidade</h3>
-            <p style="color:red">Pouco Acessível</p>
-        </div>
-    `
-});
-
-const mk = L.marker([-3.6932203,-40.3543455], {
-    icon: myIcon,
-    draggable: false
-}).addTo(map);
-
-mk.bindPopup(dv)
-*/
-
 L.control.layers({
     "Stamem": stamenLayer,
     "CartoDB": positron,
     "OpenStreetMap": openStreetMap
 }).addTo(map);
 
+/* Botoes campus */
 document.querySelector(".controls__button--mucab").onclick = ()=>{
     map.flyTo([-3.693466, -40.354933], 17.97);
     document.querySelector(".controls__button--mucab").classList.add("controls__button--selected");
